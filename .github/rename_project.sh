a260f71b0ca6718457e13d176eb936c845881c5f
ExaByt3s <ExaByt3s@users.noreply.github.com>
2023-09-20 05:28:03 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ExaByt3s"
+original_name="base_template"
+original_urlname="base_template"
+original_description="Awesome base_template created by ExaByt3s"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv base_template $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ base_template/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for base_template."""
+
+from base_template.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
